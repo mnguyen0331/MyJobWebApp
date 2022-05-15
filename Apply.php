e9cde171b41d0ab0779096fc0e876916c5cd93e4
EvanFricker <54484243+EvanFricker@users.noreply.github.com>
2022-05-14 22:06:52 -0700

    Added Apply front end stuff

--- Apply.php
+++ Apply.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<title>Application Sent!</title>
+</head>
+
+<main role="main">
+<h1>Application Sent in!</h1>
+<p>Best of luck :)</p>
+</main>
+
+<body>
+<nav role="navigation">
+<ul>
+<li><a href="index.html">Back to Main</a></li>
+<li><a href="job_posts.php">Back to Job Search</a></li>
+</ul>
+</nav>
+
+</body>
+</html>
 
--- job_posts.php
+++ job_posts.php
@@ -1,1 +1,38 @@
+<html>
+    <body>
+        <table>
+         <tr>
+            <th> Job Title </th>
+            <th> Description </th>
+            <th> Address </th>
+            <th> Qualifications </th>
+            <th> Responsibilities </th>
+            <th> Education </th>
+            <th> Job Type </th>
+            <th> Contact Details </th>
+            <th> Experience level </th>
+            <th> Salary Range </th>
+            <th> Day Posted </th>
+            <th> Deadline </th>
+            <th> Apply </th>
+        </tr>
+        <?php
+            $conn = mysqli_connect("localhost", "root", "", "myjob");
+            $sql = "SELECT * FROM Job";
+            $result = mysqli_query($conn, $sql);
 
+            if ($result->num_rows > 0) {
+                while ($row = $result-> fetch_assoc()) {
+                    echo "<tr><td>" . $row["Title"] . "</td><td>" . $row["Description"] . "</td><td>" . $row["Address"]," ",$row["City"]," ",$row["State"],", ",$row["Zipcode"]  . "</td><td>" . $row["Qualifications"] . "</td><td>" 
+                    . $row["Responsibilities"] . "</td><td>" . $row["EducationLevelId"] . "</td><td>" . $row["JobTypeId"] . "</td><td>" . $row["ContactDetaills"] 
+                    . "</td><td>" . $row["ExperienceLevelId"] . "</td><td>" . $row["SalaryRangeId"] . "</td><td>" . $row["DatePosted"] . "</td><td>" . $row["Deadline"] . "</td><td>" . "<nav role=navigation><a href=Apply.php>Apply</a></nav>" . "</td></tr>";
+                }
+            }
+            else {
+                echo "No Results";
+            }
+            $conn->close();
+        ?>
+        </table>
+    </body>
+</html>
